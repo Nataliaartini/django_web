cd301bc7ba6851281497e4e346d2e3bfcb381b83
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2022-04-13 17:21:07 -0300

    recomeçando projeto

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import index, contact, product
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
-    path('product/', product, name='product'),
-]
